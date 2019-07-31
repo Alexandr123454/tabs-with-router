d40632d30a4fefce027e12caf1232842289c7b2a
alexandr <alexandrov6927@gmail.com>
2019-07-31 16:23:52 +0300

    Updates

---
+++ precache-manifest.6cbbf872ef9de5af92a1335b28db6f7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cf96b119d1a852070af2dc421d8f93",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "114eba43f5de30619fb1",
+    "url": "/react_tabs-with-router/static/css/main.311c6757.chunk.css"
+  },
+  {
+    "revision": "a38d6ffa8972833a3633",
+    "url": "/react_tabs-with-router/static/js/2.76e8a30c.chunk.js"
+  },
+  {
+    "revision": "114eba43f5de30619fb1",
+    "url": "/react_tabs-with-router/static/js/main.b8475108.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
