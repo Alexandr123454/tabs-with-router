fc8381277edb05b2960e97e5e10ba97c06f84bd1
alexandr <alexandrov6927@gmail.com>
2019-07-31 16:48:16 +0300

    Updates

---
+++ precache-manifest.9de93cafe8fe716685e668d0fba67943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e4eb246ab351bfeec7dd3f7b3d795b",
+    "url": "/tabs-with-router/index.html"
+  },
+  {
+    "revision": "b72d4a786652ae4708fd",
+    "url": "/tabs-with-router/static/css/main.311c6757.chunk.css"
+  },
+  {
+    "revision": "a38d6ffa8972833a3633",
+    "url": "/tabs-with-router/static/js/2.76e8a30c.chunk.js"
+  },
+  {
+    "revision": "b72d4a786652ae4708fd",
+    "url": "/tabs-with-router/static/js/main.7f478404.chunk.js"
+  },
+  {
+    "revision": "3513456d47be6d2df32f",
+    "url": "/tabs-with-router/static/js/runtime~main.8e076b95.js"
+  }
+]);
